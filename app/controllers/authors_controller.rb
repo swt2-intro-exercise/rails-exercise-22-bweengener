663678337712985a0b1e037439df8784cbcd2e28
Wegener <wegener.ben@gmail.com>
2022-10-31 21:29:53 +0100

    implements creation of new authors via form

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,20 @@
 class AuthorsController < ApplicationController
   def new
   end
+
+  def show
+    @author = Author.find(params[:id])
+  end
+
+  def create
+    @article = Author.new(author_params)
+
+    @article.save
+    redirect_to @article
+  end
+
+  def author_params
+    params.require(:author).permit(:first_name, :last_name, :homepage)
+  end
+
 end
