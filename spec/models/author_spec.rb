b2a9576fb48c8f3fb8bbb01873dd1125b8f48e8c
Wegener <wegener.ben@gmail.com>
2022-10-31 21:01:21 +0100

    implements missing test #NOTICKET

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,14 +1,25 @@
+require 'rails_helper'
+
 describe "Author Model", type: :model do
   it 'should have first name, last name and homepage' do
+    first_name = "Alan"
+    last_name = "Turing"
+    homepage = "http://wikipedia.org/Alan_Turing"
 
+    author = Author.new(first_name: first_name, last_name: last_name, homepage: homepage)
+
+    expect(author.first_name).to eq(first_name)
+    expect(author.last_name).to eq(last_name)
+    expect(author.homepage).to eq(homepage)
   end
 
   it 'should have a full name getter' do
-    author = Author.new
-    author.first_name = "Alan"
-    author.last_name = "Turing"
-    author.homepage = "http://wikipedia.org/Alan_Turing"
+    first_name = "Alan"
+    last_name = "Turing"
+    homepage = "http://wikipedia.org/Alan_Turing"
 
-    expect(author.name).to eq("Alan Turing")
+    author = Author.new(first_name: first_name, last_name: last_name, homepage: homepage)
+
+    expect(author.name).to eq(first_name + " " + last_name)
   end
 end
