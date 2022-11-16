44fa83d76fa036dbea013c7f63ccc38d98a2ad33
Wegener <wegener.ben@gmail.com>
2022-11-16 11:43:56 +0100

    adds delete-support for authors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -21,6 +21,13 @@ class AuthorsController < ApplicationController
     end
   end
 
+  def destroy
+    @author = Author.find(params[:id])
+    @author.destroy
+
+    redirect_to authors_path
+  end
+
   def show
     @author = Author.find(params[:id])
   end
