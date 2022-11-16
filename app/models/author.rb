8f6d8768a5b208d42f9a455a4651e1759e8b4766
Wegener <wegener.ben@gmail.com>
2022-11-16 12:30:22 +0100

    adds multi-select of authors to paper-edit

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,6 +2,8 @@ class Author < ApplicationRecord
 
   validates :last_name, presence: true, allow_blank: false
 
+  has_and_belongs_to_many :papers
+
   def name
     self.first_name + " " + self.last_name
   end
