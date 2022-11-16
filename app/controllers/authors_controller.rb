ff9917bcc26008f356d7aa68241de84cf86f3ef9
Wegener <wegener.ben@gmail.com>
2022-11-16 11:25:38 +0100

    adds explanation for validation errors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -14,7 +14,7 @@ class AuthorsController < ApplicationController
     @author = Author.new(author_params)
 
     if @author.save
-      redirect_to @author
+      redirect_to author_path(@author)
     else
       render "new"
     end
